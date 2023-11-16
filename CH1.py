729864a278006c902cf69fdf2dda80e310533183
HB1324 <huntermbrown13@gmail.com>
2023-11-16 03:41:37 -0800

    Fixed Main File

---
+++ CH1.py
@@ -0,0 +1,55 @@
+# Chapter 1 Lab Programs
+# (Copy and Paste Only)
+
+
+#-------------------------------------
+
+# Multiplying User Input by Itself
+user_num = int(input())
+user_num_squared = user_num * user_num
+
+print(user_num_squared)
+
+#----------------------------------------
+
+# Interleaved Input / Output
+print('Enter x: ')
+x = int(input())
+
+print('x doubled is:', (2 * x))
+
+#----------------------------------------
+
+# Hello World Format
+print("Hello World!")
+
+#----------------------------------------
+
+# No Parking Sign
+print("  NO PARKING")
+print("2:00 - 6:00 a.m.")
+
+#----------------------------------------
+
+# Welcome Message
+user_name = input()
+
+print("Hey", user_name)
+print("Welcome to zyBooks!")
+
+#----------------------------------------
+
+# Input Mad Lib
+# Read a value from a user and store the value in first_name
+first_name = input()
+whole_number = input()
+plural_noun = input()
+generic_location = input()
+
+# TODO: Type your code to read three more values here.
+
+
+# Output a short story using the four input values. Do not modify the code below.
+print(first_name, 'buys', whole_number, 'different types of', plural_noun, 'at', generic_location)
+
+#----------------------------------------
