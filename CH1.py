5cf4b5e0cc96ee843b93b075c0ce8b949254644d
HB1324 <huntermbrown13@gmail.com>
2023-11-27 10:03:43 -0800

    version 1.0 (11/27/23)

--- CH1.py
+++ CH1.py
@@ -41,10 +41,10 @@ print("Welcome to zyBooks!")
 
 # Input Mad Lib
 # Read a value from a user and store the value in first_name
-first_name = input()
-whole_number = input()
-plural_noun = input()
-generic_location = input()
+first_name = input("Enter First Name: ")
+whole_number = int(input("Enter Whole Number: "))
+plural_noun = input("Enter Plural Noun: ")
+generic_location = input("Enter Generic Location: ")
 
 # TODO: Type your code to read three more values here.
 
